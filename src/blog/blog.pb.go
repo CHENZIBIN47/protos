5370beac8db773f3fbc1f33cb09fe810a0913671
chenzibin <chenzb@ffcs.cn>
2021-08-06 13:03:34 +0800

    fix

--- src/blog/blog.pb.go
+++ src/blog/blog.pb.go
@@ -8,7 +8,7 @@ package blog
 
 import (
 	context "context"
-	_ "github.com/jergoo/go-grpc-example/proto/google/api"
+	_ "github.com/CHENZIBIN47/protos/src/google/api"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
